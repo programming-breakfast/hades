0f83557968874bf1981dffe6e0d3b6ea6373cd81
Anton Sidorov <anton@ubernil.com>
2015-08-02 14:50:08 +0700

    add cerberus

--- lib/hades.ex
+++ lib/hades.ex
@@ -8,8 +8,9 @@ defmodule Hades do
 
     children = [
       # Start the endpoint when the application starts
-      supervisor(Hades.Endpoint, [])
+      supervisor(Hades.Endpoint, []),
       # Here you could define other workers and supervisors as children
+      worker(Hades.Cerberus, [])
       # worker(Hades.Worker, [arg1, arg2, arg3]),
     ]
 
---
+++ lib/hades/cerberus.ex
@@ -0,0 +1,46 @@
+defmodule Hades.Cerberus do
+  use GenServer
+
+  def start_link(opts \\ [name: __MODULE__]) do
+    GenServer.start_link(__MODULE__, [], opts)
+  end
+
+  #
+  # Client API
+  #
+
+  def list do
+    GenServer.call(__MODULE__, :list)
+  end
+
+  def show(name) do
+    GenServer.call(__MODULE__, {:show, name})
+  end
+
+  #
+  # Server callbacks
+  #
+
+  def handle_call(:list, _from, state) do
+    result = "HEY, MAMA!"
+    {:reply, result, state}
+  end
+
+  def handle_call({:show, name}, _from, state) do
+    result = state[:processes][name]
+
+    {:reply, result, state}
+  end
+
+
+  def init(_) do
+    state = HashDict.new
+      |> Dict.put(
+           :processes,
+           %{"foo" =>
+           %{name: "foo", description: "foo description"}
+         })
+
+    {:ok, state}
+  end
+end
