6f6a9891ad1bf4c5e2f67c8b2a8b3a5966b77dc4
Anton Sidorov <anton@ubernil.com>
2015-08-02 14:50:58 +0700

    add :runtime_tools to make observer work

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Hades.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {Hades, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger]]
+     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :runtime_tools]]
   end
 
   # Specifies which paths to compile per environment
