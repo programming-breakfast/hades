9ae34c4a337bef0b4204d2e70bb965a709b2482f
Anton Sidorov <anton@ubernil.com>
2015-08-02 20:24:44 +0700

    Ma, look, they are alive

--- lib/hades/cerberus.ex
+++ lib/hades/cerberus.ex
@@ -1,5 +1,6 @@
 defmodule Hades.Cerberus do
   use GenServer
+  import Logger
 
   alias Hades.Cerberus.Soul
 
@@ -25,7 +26,7 @@ defmodule Hades.Cerberus do
 
   def handle_call(:list, _from, state) do
     result = :ets.tab2list(__MODULE__)
-    |> Enum.map(fn {_, soul} -> soul end)
+    |> Enum.map(fn {_, _, soul} -> soul end)
 
     IO.inspect result
 
@@ -33,12 +34,16 @@ defmodule Hades.Cerberus do
   end
 
   def handle_call({:show, name}, _from, state) do
-    [{_, soul} | _] = :ets.lookup(__MODULE__, name)
+    [{_, _, soul} | _] = :ets.lookup(__MODULE__, name)
     {:reply, soul, state}
   end
 
-  def handle_info(any, _from, state) do
-    IO.inspect any
+  def handle_info({:DOWN, _ref, :process, pid, message}, state) do
+    [[soul] | _] = :ets.match(__MODULE__, {:'_', pid, :'$1'})
+    Logger.warn "Soul #{soul.name} exited with #{inspect message}. Restarting."
+
+    start_soul(soul)
+
     {:noreply, state}
   end
 
@@ -61,17 +66,19 @@ defmodule Hades.Cerberus do
     |> Enum.each(&start_soul(&1))
   end
 
-  @soul_startup_options []
+  @soul_startup_options [:monitor]
 
   defp start_soul(soul) do
-    soul = case :exec.run_link(String.to_char_list(soul.start), @soul_startup_options) do
-      {:ok, _pid, os_pid} ->
-        %Soul{soul | os_pid: os_pid, state: :running, timer: 1}
+    Logger.info "Starting external process #{soul.name}."
+
+    soul = case :exec.run(String.to_char_list(soul.start), @soul_startup_options) do
+      {:ok, pid, os_pid} ->
+        %Soul{soul | os_pid: os_pid, pid: pid, state: :running, timer: 1}
       {_, _, _} ->
         %Soul{soul | state: :startup_error, timer: 1}
     end
 
-    :ets.insert(__MODULE__, {soul.name, soul})
+    :ets.insert(__MODULE__, {soul.name, soul.pid, soul})
   end
 
   defp init_ets do
--- lib/hades/cerberus/soul.ex
+++ lib/hades/cerberus/soul.ex
@@ -1,4 +1,4 @@
 defmodule Hades.Cerberus.Soul do
   defstruct name: nil, description: nil, start: nil, stop: nil, restart: nil,
-    stop_timeout: nil, os_pid: nil, state: nil, timer: :infinity
+    stop_timeout: nil, os_pid: nil, pid: nil, state: nil, timer: :infinity
 end
