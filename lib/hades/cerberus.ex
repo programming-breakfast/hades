625445df977a9b7a434a6a31dba66ca3cb7df201
Timur Kozmenko <timraell@gmail.com>
2015-08-14 18:08:33 +0700

    feat(cerberus): introduce basic stop

--- lib/hades/cerberus.ex
+++ lib/hades/cerberus.ex
@@ -20,6 +20,10 @@ defmodule Hades.Cerberus do
     GenServer.call(__MODULE__, {:show, name})
   end
 
+  def stop(name) do
+    GenServer.call(__MODULE__, {:stop, name})
+  end
+
   #
   # Server callbacks
   #
@@ -38,12 +42,19 @@ defmodule Hades.Cerberus do
     {:reply, soul, state}
   end
 
+  def handle_call({:stop, name}, _from, state) do
+    [{_, _, soul} | _] = :ets.lookup(__MODULE__, name)
+    stop_soul(soul)
+    {:reply, soul, state}
+  end
+
   def handle_info({:DOWN, _ref, :process, pid, message}, state) do
     soul = find_soul(pid)
+    Logger.warn "Soul #{inspect soul} down."
     case soul.state do
       :trying_to_stop ->
         Logger.warn "Soul #{soul.name} successfully stopped."
-        update_soul(soul, %{state: 'stopped'})
+        update_soul(soul, %{state: :stopped, os_pid: nil, pid: nil})
       _ ->
         Logger.warn "Soul #{soul.name} exited with #{inspect message}. Restarting."
         start_soul(soul)
@@ -55,8 +66,8 @@ defmodule Hades.Cerberus do
 
   def init(_) do
     config = [
-      %Soul{name: "ping-ya", description: "foo description", start: "ping ya.ru", stop: "kill -9 %pid%"},
-      %Soul{name: "ping-google", description: "foo description", start: "ping google.com"}
+      %Soul{name: "ping-ya", description: "foo description", start: "ping ya.ru"},
+      %Soul{name: "ping-google", description: "foo description", start: "ping google.ru"}
     ]
 
     init_ets
@@ -83,10 +94,20 @@ defmodule Hades.Cerberus do
 
   @soul_startup_options [:monitor]
 
-  defp start_soul(soul) do
-    Logger.info "Starting external process #{soul.name}."
+  defp soul_startup_options(soul) do
+    startup_options = case soul.stop do
+      nil ->
+        []
+      stop_command ->
+        [{:kill, String.to_char_list(stop_command)}]
+    end
 
-    case :exec.run(String.to_char_list(soul.start), @soul_startup_options) do
+    startup_options ++ @soul_startup_options
+  end
+
+  defp start_soul(soul) do
+    Logger.info "Starting external process #{soul.name} with suct options: #{inspect soul_startup_options(soul)}."
+    case :exec.run(String.to_char_list(soul.start), soul_startup_options(soul)) do
       {:ok, pid, os_pid} ->
         update_soul(soul, %{os_pid: os_pid, pid: pid, state: :running, timer: 1})
       {_, _, _} ->
@@ -95,11 +116,13 @@ defmodule Hades.Cerberus do
   end
 
   defp stop_soul(soul) do
-    update_soul(%{state: :trying_to_stop})
+    update_soul(soul, %{state: :trying_to_stop})
+    :exec.stop(soul.pid)
   end
 
   defp update_soul(soul, soul_attrs) do
-    :ets.insert(__MODULE__, {soul.name, soul.pid, Map.merge(soul, soul_attrs)})
+    updated_soul = Map.merge(soul, soul_attrs)
+    :ets.insert(__MODULE__, {updated_soul.name, updated_soul.pid, updated_soul})
   end
 
   defp init_ets do
