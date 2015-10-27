56249af4a8292ec516d71304525d094557756702
Timur Kozmenko <timraell@gmail.com>
2015-10-27 11:11:55 +0700

    minor_fix(deploy)

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh -A aviasales@wwa.int.avs.io << EOF
   cd $APP_PATH
   git checkout .
   git pull --rebase
-  mix release.clean && mix deps.get --only prod && brunch build --production && MIX_ENV=prod mix phoenix.digest && MIX_ENV=prod mix release && scp rel/hades/releases/0.0.2/hades.tar.gz aviasales@yasen2.int.avs.io:~/hades/hades.tar.gz
+  mix deps.get && brunch build --production && MIX_ENV=prod mix phoenix.digest && mix release.clean && MIX_ENV=prod mix release && scp rel/hades/releases/0.0.2/hades.tar.gz aviasales@yasen2.int.avs.io:~/hades/hades.tar.gz && scp mon/monitoring.py aviasales@yasen2.int.avs.io:~/hades/monitoring.py
 EOF
 
 
